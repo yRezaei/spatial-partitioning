41103d72655c51ba3f20d4c160f7fe5a842700ab
Yashar Rezaei <yashar.rezaei@tarja.com>
2019-03-12 15:52:17 +0100

    CMakeLists modifyed to destribute the executable into <platform> foldder

---
+++ build/CMakeFiles/spatial-partitioning.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/github/spatial-partitioning/src/main.cpp" "D:/github/spatial-partitioning/build/CMakeFiles/spatial-partitioning.dir/src/main.cpp.obj"
+  "D:/github/spatial-partitioning/src/quadtree.cpp" "D:/github/spatial-partitioning/build/CMakeFiles/spatial-partitioning.dir/src/quadtree.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external_lib/glm/include"
+  "../external_lib/sfml/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/spatial-partitioning.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spatial-partitioning.dir/src/quadtree.cpp.obj"
+  "CMakeFiles/spatial-partitioning.dir/src/main.cpp.obj"
+  "../bin/Windows/Release/spatial-partitioning.pdb"
+  "../bin/Windows/Release/spatial-partitioning.exe"
+  "../bin/Windows/Release/spatial-partitioning.exe.manifest"
+  "../bin/Windows/Release/libspatial-partitioning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spatial-partitioning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
